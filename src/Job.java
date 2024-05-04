4d25ca57c2ea2668a90d91078b92c413412d396f
Eda Kırcı <edakirci@gmail.com>
2024-05-04 18:07:31 +0300

    constructers

--- src/Job.java
+++ src/Job.java
@@ -2,5 +2,9 @@ public class Job {
     private int jobID;
     private double jobSize;
 
+    public Job(int jobID, double jobSize) {
+        this.jobID = jobID;
+        this.jobSize = jobSize;
+    }
 }
 
