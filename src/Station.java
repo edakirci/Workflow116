7d5deab47ee02b32eff348088fdc227ebe5db4d3
Eda Kırcı <edakirci@gmail.com>
2024-05-07 23:02:52 +0300

    fixing errors in the Station

--- src/Station.java
+++ src/Station.java
@@ -1,4 +1,8 @@
-public class Station extends AbstractStation{
-    //emre
+import java.util.List;
+import java.util.PriorityQueue;
 
+public class Station extends AbstractStation{
+    public Station(String taskType, int maxCapacity, boolean multiflag, boolean fifoflag, List<TaskType> currentTasks, PriorityQueue<TaskType> taskQueue) {
+        super(taskType, maxCapacity, multiflag, fifoflag, currentTasks, taskQueue);
+    }
 }
