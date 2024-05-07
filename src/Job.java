a9eafd62ff8d5c900fd43f4b5bff9730f763bf0c
Eda Kırcı <edakirci@gmail.com>
2024-05-07 21:49:15 +0300

    creating the classes

--- src/Job.java
+++ src/Job.java
@@ -1,25 +1,4 @@
 public class Job {
-    private int jobtypeID;
-    private double jobSize;
 
-    public Job(int jobtypeID, double jobSize) {
-        this.jobtypeID = jobtypeID;
-        this.jobSize = jobSize;
-    }
 
-    public int getJobtypeID() {
-        return jobtypeID;
-    }
-
-    public void setJobtypeID(int jobtypeID) {
-        this.jobtypeID = jobtypeID;
-    }
-
-    public double getJobSize() {
-        return jobSize;
-    }
-
-    public void setJobSize(double jobSize) {
-        this.jobSize = jobSize;
-    }
 }
